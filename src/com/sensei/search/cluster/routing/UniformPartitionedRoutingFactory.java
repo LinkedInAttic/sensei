4cfa55dc4d8083723b3801d4356c30f7426ec07b
Neha Narkhede <nnarkhed@nnarkhed-md.linkedin.biz>
2010-04-15 13:53:52 -0700

    norbert upgrade , tests not working

---
+++ src/com/sensei/search/cluster/routing/UniformPartitionedRoutingFactory.java
@@ -0,0 +1,59 @@
+package com.sensei.search.cluster.routing;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.linkedin.norbert.cluster.InvalidClusterException;
+import com.linkedin.norbert.cluster.Node;
+import com.linkedin.norbert.network.javaapi.PartitionedLoadBalancer;
+import com.linkedin.norbert.network.javaapi.PartitionedLoadBalancerFactory;
+
+public class UniformPartitionedRoutingFactory implements PartitionedLoadBalancerFactory<Integer> {
+	private final Random _rand = new Random(System.nanoTime());
+	
+//	public PartitionedLoadBalancer newRouter(Node[] nodes) throws InvalidClusterException {
+//		final Int2ObjectMap<ArrayList<Node>> nodeMap = new Int2ObjectOpenHashMap<ArrayList<Node>>();
+//		IntSet parts = new IntOpenHashSet();
+//		for (Node node : nodes){
+//			int[] partitions = node.getPartitions();
+//			for (int partition : partitions){
+//				parts.add(partition);
+//				ArrayList<Node> nodeList = nodeMap.get(partition);
+//				if (nodeList==null){
+//					nodeList=new ArrayList<Node>(nodes.length);
+//					nodeMap.put(partition, nodeList);
+//				}
+//				nodeList.add(node);
+//			}
+//		}
+//		
+//		return new UniformPartitionedLoadBalancer(nodeMap,_rand);
+//	}
+
+  /* (non-Javadoc)
+   * @see com.linkedin.norbert.network.javaapi.PartitionedLoadBalancerFactory#newLoadBalancer(com.linkedin.norbert.cluster.Node[])
+   */
+  public PartitionedLoadBalancer<Integer> newLoadBalancer(Node[] nodes) throws InvalidClusterException
+  {
+    final Int2ObjectMap<ArrayList<Node>> nodeMap = new Int2ObjectOpenHashMap<ArrayList<Node>>();
+    IntSet parts = new IntOpenHashSet();
+    for (Node node : nodes){
+        int[] partitions = node.getPartitions();
+        for (int partition : partitions){
+            parts.add(partition);
+            ArrayList<Node> nodeList = nodeMap.get(partition);
+            if (nodeList==null){
+                nodeList=new ArrayList<Node>(nodes.length);
+                nodeMap.put(partition, nodeList);
+            }
+            nodeList.add(node);
+        }
+    }    
+    return new UniformPartitionedLoadBalancer(nodeMap,_rand);
+  }
+}
