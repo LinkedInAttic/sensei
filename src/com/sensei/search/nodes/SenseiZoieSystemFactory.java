cdae5684f53ab19715167dc221a5dbd524be7195
John Wang <jwang@jwang-mn.local>
2010-06-03 22:20:20 -0700

    allow for intial empty indexes

--- src/com/sensei/search/nodes/SenseiZoieSystemFactory.java
+++ src/com/sensei/search/nodes/SenseiZoieSystemFactory.java
@@ -3,6 +3,8 @@ package com.sensei.search.nodes;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import org.apache.log4j.Logger;
+
 import proj.zoie.api.indexing.IndexReaderDecorator;
 import proj.zoie.api.indexing.ZoieIndexableInterpreter;
 import proj.zoie.impl.indexing.ZoieConfig;
@@ -12,6 +14,7 @@ import com.browseengine.bobo.api.BoboIndexReader;
 
 public class SenseiZoieSystemFactory<V>
 {
+  private static Logger log = Logger.getLogger(SenseiZoieSystemFactory.class);
   protected File _idxDir;
   protected ZoieIndexableInterpreter<V> _interpreter;
   protected IndexReaderDecorator<BoboIndexReader> _indexReaderDecorator;
@@ -32,9 +35,10 @@ public class SenseiZoieSystemFactory<V>
     File partDir = getPath(partitionId);
     if(!partDir.exists())
     {
-      throw new FileNotFoundException("partition=" + partitionId + " does not exist");
+      partDir.mkdirs();
+      log.info("partition=" + partitionId + " does not exist, directory created.");
     }
-    return new ZoieSystem<BoboIndexReader,V>(getPath(partitionId), _interpreter, _indexReaderDecorator, _zoieConfig);
+    return new ZoieSystem<BoboIndexReader,V>(partDir, _interpreter, _indexReaderDecorator, _zoieConfig);
   }
   
   // TODO: change to getDirectoryManager
