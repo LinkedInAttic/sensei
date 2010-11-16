b009163f0a1f0b012790262a4806991b6494fd93
John Wang <jwang@jwang-mn.local>
2010-11-16 09:15:47 -0800

    add string query impl

--- src/com/sensei/search/req/SenseiQuery.java
+++ src/com/sensei/search/req/SenseiQuery.java
@@ -1,18 +1,22 @@
 package com.sensei.search.req;
 
-public final class SenseiQuery {
+import java.nio.charset.Charset;
+
+public class SenseiQuery {
   private byte[] _bytes;
+  static Charset utf8Charset = Charset.forName("UTF-8");
+	
   public SenseiQuery(byte[] bytes){
 	  _bytes = bytes;
   }
   
-  public byte[] toBytes(){
+  final public byte[] toBytes(){
 	  return _bytes;
   }
   
   @Override
   public String toString()
   {
-    return new String(_bytes);
+	return new String(_bytes,utf8Charset);
   }
 }
