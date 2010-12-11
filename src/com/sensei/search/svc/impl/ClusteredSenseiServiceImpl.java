fe47e3e5c45106ff0e80ac52d844f4172e8ff111
John <john.wang@gmail.com>
2010-12-10 23:40:33 -0800

    fixed up cluster service impl

--- src/com/sensei/search/svc/impl/ClusteredSenseiServiceImpl.java
+++ src/com/sensei/search/svc/impl/ClusteredSenseiServiceImpl.java
@@ -2,40 +2,81 @@ package com.sensei.search.svc.impl;
 
 import org.apache.log4j.Logger;
 
-import com.linkedin.norbert.cluster.ClusterShutdownException;
 import com.linkedin.norbert.javacompat.cluster.ClusterClient;
+import com.linkedin.norbert.javacompat.cluster.ZooKeeperClusterClient;
+import com.linkedin.norbert.javacompat.network.NetworkClientConfig;
+import com.sensei.search.cluster.client.SenseiNetworkClient;
+import com.sensei.search.cluster.routing.UniformPartitionedRoutingFactory;
 import com.sensei.search.nodes.SenseiBroker;
+import com.sensei.search.nodes.impl.NoopRequestScatterRewriter;
 import com.sensei.search.req.SenseiRequest;
 import com.sensei.search.req.SenseiResult;
 import com.sensei.search.svc.api.SenseiException;
 import com.sensei.search.svc.api.SenseiService;
 
-public class ClusteredSenseiServiceImpl implements SenseiService {
-	private static Logger log = Logger.getLogger(ClusteredSenseiServiceImpl.class);
+public class ClusteredSenseiServiceImpl implements SenseiService {	
+	private static final Logger logger = Logger.getLogger(ClusteredSenseiServiceImpl.class);
 
-	private SenseiBroker _broker;
-	private ClusterClient _cluster;
+	private final UniformPartitionedRoutingFactory _routerFactory = new UniformPartitionedRoutingFactory();
+	private final NoopRequestScatterRewriter _reqRewriter = new NoopRequestScatterRewriter();
+	private final NetworkClientConfig _networkClientConfig = new NetworkClientConfig();
+	
+	private SenseiBroker _senseiBroker;
+	private SenseiNetworkClient _networkClient = null;
+	private ClusterClient _clusterClient;
+	private final String _clusterName;
+	
+	public ClusteredSenseiServiceImpl(String zkurl,int zkTimeout,String clusterName){
+		_clusterName = clusterName;
+		_networkClientConfig.setServiceName(clusterName);
+	    _networkClientConfig.setZooKeeperConnectString(zkurl);
+	    _networkClientConfig.setZooKeeperSessionTimeoutMillis(zkTimeout);
+	    _networkClientConfig.setConnectTimeoutMillis(1000);
+	    _networkClientConfig.setWriteTimeoutMillis(150);
+	    _networkClientConfig.setMaxConnectionsPerNode(5);
+	    _networkClientConfig.setStaleRequestTimeoutMins(10);
+	    _networkClientConfig.setStaleRequestCleanupFrequencyMins(10);
+	    
+	    _clusterClient = new ZooKeeperClusterClient(clusterName,zkurl,zkTimeout);
+		
+	    _networkClientConfig.setClusterClient(_clusterClient);
+		
+		_networkClient = new SenseiNetworkClient(_networkClientConfig,_routerFactory);
+		_senseiBroker = new SenseiBroker(_networkClient, _clusterClient, _reqRewriter, _routerFactory);
+	}
+	
+	public void start(){
+		logger.info("Connecting to cluster: "+_clusterName+" ...");
+		_clusterClient.awaitConnectionUninterruptibly();
+
+		logger.info("Cluster: "+_clusterName+" successfully connected ");
+	}
 	
 	public SenseiResult doQuery(SenseiRequest req) throws SenseiException {
-		return _broker.browse(req);
+		return _senseiBroker.browse(req);
 	}
 	
 	public void shutdown(){
 		try{
-		  log.info("shutting down client...");
-		  _broker.shutdown();
-		}
-		finally{
-		  log.info("shutting down cluster...");
-          try{
-            _cluster.shutdown();
-          } catch (ClusterShutdownException e) {
-        	  log.error(e.getMessage(),e);
-		}
-          finally{
-          }
-          
-		}
+		    if (_senseiBroker!=null){
+			  _senseiBroker.shutdown();
+			  _senseiBroker = null;
+		    }
+		  }
+		  finally{
+			  try{
+			    if (_networkClient!=null){
+			    	_networkClient.shutdown();
+			    	_networkClient = null;
+			    }
+			  }
+			  finally{
+				if (_clusterClient!=null){
+					_clusterClient.shutdown();
+					_clusterClient = null;
+				}
+			  }
+		  }
 	}
 
 }
