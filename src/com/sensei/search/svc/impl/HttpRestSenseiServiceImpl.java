a932dcdff2e55c84a07d7f225582a55d59d300d4
John Wang <jwang@jwang-md.linkedin.biz>
2011-01-07 14:36:23 -0800

    started rest svc

---
+++ src/com/sensei/search/svc/impl/HttpRestSenseiServiceImpl.java
@@ -0,0 +1,23 @@
+package com.sensei.search.svc.impl;
+
+import com.sensei.search.req.SenseiRequest;
+import com.sensei.search.req.SenseiResult;
+import com.sensei.search.req.SenseiSystemInfo;
+import com.sensei.search.svc.api.SenseiException;
+import com.sensei.search.svc.api.SenseiService;
+
+public class HttpRestSenseiServiceImpl implements SenseiService {
+
+	@Override
+	public SenseiResult doQuery(SenseiRequest req) throws SenseiException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SenseiSystemInfo getSystemInfo() throws SenseiException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
