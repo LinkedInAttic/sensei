c9806fb1a81448819ea34475f018dbe5b3307b37
John Wang <john.wang@gmail.com>
2011-03-31 01:20:09 -0700

    added default interpreter and started on default indexing manager

---
+++ src/main/java/com/sensei/indexing/api/DefaultStreamingIndexingManager.java
@@ -0,0 +1,32 @@
+package com.sensei.indexing.api;
+
+import java.util.Map;
+
+import proj.zoie.api.Zoie;
+
+import com.browseengine.bobo.api.BoboIndexReader;
+import com.sensei.search.nodes.SenseiIndexingManager;
+
+public class DefaultStreamingIndexingManager implements SenseiIndexingManager {
+
+	@Override
+	public void initialize(
+			Map<Integer, Zoie<BoboIndexReader, ?, ?>> zoieSystemMap)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
