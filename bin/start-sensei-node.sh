5fa92b9ff632fd89738d4e57fd888dfb6371df7f
Baoqiu Cui <bcui@bcui-2-md.linkedin.biz>
2011-04-11 15:52:48 -0700

    Fixed a minor problem in start-sensei-node.sh to exclude IPs of VMs.

--- bin/start-sensei-node.sh
+++ bin/start-sensei-node.sh
@@ -12,10 +12,10 @@ bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 
 OS=`uname`
-IO="" # store IP
+IP="" # store IP
 case $OS in
    Linux) IP=`/sbin/ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`;;
-   FreeBSD|OpenBSD|Darwin) IP=`ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'` ;;
+   FreeBSD|OpenBSD|Darwin) IP=`ifconfig | grep -E '^en[0-9]:' -A 4 | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'` ;;
    SunOS) IP=`ifconfig -a | grep inet | grep -v '127.0.0.1' | awk '{ print $2} '` ;;
    *) IP="Unknown";;
 esac
