931a8dfee00a8e1fdc35db16da43af5ae8c4928a
John Wang <john.wang@gmail.com>
2011-11-10 19:38:59 -0800

    bug fix: SENSEI-66

--- sensei-core/src/main/java/com/sensei/search/req/AbstractSenseiRequest.java
+++ sensei-core/src/main/java/com/sensei/search/req/AbstractSenseiRequest.java
@@ -5,7 +5,9 @@ import java.util.Set;
 
 public interface AbstractSenseiRequest extends Serializable
 {
-  public abstract void setPartitions(Set<Integer> partitions);
-  public abstract Set<Integer> getPartitions();
-  public abstract String getRouteParam();
+  public void setPartitions(Set<Integer> partitions);
+  public Set<Integer> getPartitions();
+  public String getRouteParam();
+  public void saveState();
+  public void restoreState();
 }
