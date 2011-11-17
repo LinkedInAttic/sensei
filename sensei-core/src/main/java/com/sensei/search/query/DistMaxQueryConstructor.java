56074d66e7e7028f2c64ae658b37e4b60c6b356f
Sheng Guo <enigmaguo@hotmail.com>
2011-11-17 00:26:22 -0800

    added DistMaxQueryConstructor

--- sensei-core/src/main/java/com/sensei/search/query/DistMaxQueryConstructor.java
+++ sensei-core/src/main/java/com/sensei/search/query/DistMaxQueryConstructor.java
@@ -1,13 +1,19 @@
 package com.sensei.search.query;
 
+import java.util.ArrayList;
+
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.DisjunctionMaxQuery;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DistMaxQueryConstructor extends QueryConstructor {
 
 	@Override
-	public Query constructQuery(JSONObject params) throws JSONException {
+	public Query constructQuery(JSONObject jsonQuery) throws JSONException {
 
 //	    "dis_max" : {
 //        "tie_breaker" : 0.7,
@@ -22,7 +28,22 @@ public class DistMaxQueryConstructor extends QueryConstructor {
 //        ]
 //    },
 		
-		return null;
+	   JSONArray jsonArray = jsonQuery.getJSONArray("queries");
+	   ArrayList<Query> ar = new ArrayList<Query>();
+	   
+	    for(int i = 0; i<jsonArray.length(); i++){
+	      JSONObject json = jsonArray.getJSONObject(i).getJSONObject("term");
+	      String field = (String)(json.keys().next());
+	      String value = (String)json.get(field);
+	      ar.add(new TermQuery(new Term(field, value)));
+	    }
+	    
+	    float tieBreakerMultiplier = (float) jsonQuery.optDouble("tie_breaker", 0.7);
+	    float boost = (float) jsonQuery.optDouble("boost", 1.2);
+	    Query dmq = new DisjunctionMaxQuery(ar, tieBreakerMultiplier);
+	    dmq.setBoost(boost);
+	    
+		return dmq;
 	}
 
 }
