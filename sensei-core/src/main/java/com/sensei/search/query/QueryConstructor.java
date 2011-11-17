80d5611c49304e3be307e134f0b12ab7f2434e62
Lei Wang <liwang@linkedin.com>
2011-11-17 14:02:36 -0800

    TermQueryConstructor implementation.

--- sensei-core/src/main/java/com/sensei/search/query/QueryConstructor.java
+++ sensei-core/src/main/java/com/sensei/search/query/QueryConstructor.java
@@ -1,46 +1,148 @@
 package com.sensei.search.query;
 
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
 import org.json.JSONObject;
 
-public abstract class QueryConstructor {
-	abstract public Query constructQuery(JSONObject params);
-	
-	public static QueryConstructor getQueryConstructor(String type){
-		return null;
-	}
-	
-	public class TermQueryConstructor extends QueryConstructor{
+public abstract class QueryConstructor
+{
+  abstract public Query constructQuery(JSONObject params);
+  
+  public static QueryConstructor getQueryConstructor(String type)
+  {
+    if ("match_all".equals(type))
+    {
+      return new MatchAllQueryConstructor();
+    }
+    else if ("ids".equals(type))
+    {
+    }
+    else if ("query_string".equals(type))
+    {
+    }
+    else if ("text".equals(type))
+    {
+      return new TextQueryConstructor();
+    }
+    else if ("term".equals(type))
+    {
+      return new TermQueryConstructor();
+    }
+    else if ("bool".equals(type))
+    {
+    }
+    else if ("dis_max".equals(type))
+    {
+    }
+    else if ("prefix".equals(type))
+    {
+    }
+    else if ("wildcard".equals(type))
+    {
+    }
+    else if ("range".equals(type))
+    {
+    }
+    else if ("filtered".equals(type))
+    {
+    }
+    else if ("terms".equals(type))
+    {
+    }
+    else if ("span_term".equals(type))
+    {
+    }
+    else if ("span_or".equals(type))
+    {
+    }
+    else if ("span_not".equals(type))
+    {
+    }
+    else if ("span_near".equals(type))
+    {
+    }
+    else if ("span_first".equals(type))
+    {
+    }
+    else if ("custom".equals(type))
+    {
+    }
 
-		@Override
-		public Query constructQuery(JSONObject params) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	}
-	
-	public static class MatchAllQueryConstructor extends QueryConstructor{
+    throw new IllegalArgumentException("query type: " + type);
+  }
+  
+  public static class TermQueryConstructor extends QueryConstructor
+  {
+    @Override
+    public Query constructQuery(JSONObject params)
+    {
+      if (params == null)
+        return null;
 
-		@Override
-		public Query constructQuery(JSONObject params) {
-			double boost = params.optDouble("boost",1.0);
-			
-			MatchAllDocsQuery q = new MatchAllDocsQuery();
-			q.setBoost((float)boost);
-			
-			return q;
-		}
-		
-	}
-	
-	public static class TextQueryConstructor extends QueryConstructor{
+      BooleanQuery bq = null;
+      Query query = null;
 
-		@Override
-		public Query constructQuery(JSONObject params) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-	}
+      for (String field : JSONObject.getNames(params))
+      {
+        Object obj = params.opt(field);
+        String txt;
+        float boost;
+        if (obj == null)
+          throw new IllegalArgumentException("no term value specified: " + params);
+        else if (obj instanceof JSONObject)
+        {
+          txt = ((JSONObject)obj).optString("term");
+          if (txt == null || txt.length() == 0)
+            throw new IllegalArgumentException("no term value specified: " + params);
+          boost = (float)((JSONObject)obj).optDouble("boost", 1.0);
+        }
+        else
+        {
+          txt   = String.valueOf(obj);
+          boost = 1.0f;
+        }
+        Query q = new TermQuery(new Term(field, txt));
+        q.setBoost(boost);
+        if (query == null)
+          query = q;
+        else
+        {
+          if (bq == null)
+            bq = new BooleanQuery();
+          bq.add(q, BooleanClause.Occur.MUST);
+        }
+      }
+
+      if (bq != null)
+        return bq;
+      return query;
+    }
+  }
+  
+  public static class MatchAllQueryConstructor extends QueryConstructor
+  {
+    @Override
+    public Query constructQuery(JSONObject params) {
+      double boost = params.optDouble("boost",1.0);
+      
+      MatchAllDocsQuery q = new MatchAllDocsQuery();
+      q.setBoost((float)boost);
+      
+      return q;
+    }
+  }
+  
+  public static class TextQueryConstructor extends QueryConstructor
+  {
+    @Override
+    public Query constructQuery(JSONObject params)
+    {
+      // TODO Auto-generated method stub
+      return null;
+    }
+  }
 }
