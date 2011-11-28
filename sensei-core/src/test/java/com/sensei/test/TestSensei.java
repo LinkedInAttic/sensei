0d85a8de7ad05295731bcfe0b3d4c4e3b9de364d
Lei Wang <liwang@linkedin.com>
2011-11-28 14:31:26 -0800

    More json api tests.

--- sensei-core/src/test/java/com/sensei/test/TestSensei.java
+++ sensei-core/src/test/java/com/sensei/test/TestSensei.java
@@ -529,6 +529,24 @@ public class TestSensei extends AbstractSenseiTestCase
     assertEquals("numhits is wrong", 15000, res.getInt("numhits"));
   }
 
+  public void testUIDQuery() throws Exception
+  {
+    logger.info("executing test case testUIDQuery");
+    String req = "{\"query\": {\"ids\": {\"values\": [\"1\", \"2\", \"3\"], \"excludes\": [\"2\"]}}}";
+    JSONObject res = search(new JSONObject(req));
+    assertEquals("numhits is wrong", 2, res.getInt("numhits"));
+    assertEquals("the first uid is wrong", 1, res.getJSONArray("hits").getJSONObject(0).getInt("uid"));
+    assertEquals("the second uid is wrong", 3, res.getJSONArray("hits").getJSONObject(1).getInt("uid"));
+  }
+
+  public void testTextQuery() throws Exception
+  {
+    logger.info("executing test case testTextQuery");
+    String req = "{\"query\": {\"text\": {\"contents\": \"red cool\", \"operator\": \"and\"}}}";
+    JSONObject res = search(new JSONObject(req));
+    assertEquals("numhits is wrong", 1070, res.getInt("numhits"));
+  }
+
   /**
    * @param res
    *          result
