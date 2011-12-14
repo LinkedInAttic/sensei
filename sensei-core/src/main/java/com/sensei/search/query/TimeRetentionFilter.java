184a86b8c15a6f76431eb8d1e7769532f5a37b29
John Wang <john.wang@gmail.com>
2011-12-13 19:28:51 -0800

    fixed retention filter

--- sensei-core/src/main/java/com/sensei/search/query/TimeRetentionFilter.java
+++ sensei-core/src/main/java/com/sensei/search/query/TimeRetentionFilter.java
@@ -2,15 +2,20 @@ package com.sensei.search.query;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.DocIdSet;
 import org.apache.lucene.search.Filter;
 
+import proj.zoie.api.ZoieIndexReader;
+
 import com.browseengine.bobo.api.BoboIndexReader;
 import com.browseengine.bobo.api.BrowseSelection;
 import com.browseengine.bobo.facets.FacetHandler;
+import com.kamikaze.docidset.impl.OrDocIdSet;
 import com.sensei.indexing.api.DefaultSenseiInterpreter;
 import com.sensei.indexing.api.MetaType;
 
@@ -29,25 +34,36 @@ public class TimeRetentionFilter extends Filter {
     _nDays = nDays;
     _dataUnit = dataUnit;
   }
+  
+  private DocIdSet buildFilterSet(BoboIndexReader boboReader) throws IOException{
+    FacetHandler facetHandler = boboReader.getFacetHandler(_column);
+    
+    if (facetHandler!=null){
+      DecimalFormat formatter = new DecimalFormat(DefaultSenseiInterpreter.DEFAULT_FORMAT_STRING_MAP.get(MetaType.Long));
+      BrowseSelection sel = new BrowseSelection(_column);
+      long duration = _dataUnit.convert(_nDays, TimeUnit.DAYS);
+      long now = _dataUnit.convert(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+      long from = now - duration;
+      sel.addValue("["+formatter.format(from)+" TO *]");
+      return facetHandler.buildFilter(sel).getDocIdSet(boboReader);
+    }
+    throw new IllegalStateException("no facet handler defined with column: "+_column);
+  }
+  
   @Override
   public DocIdSet getDocIdSet(IndexReader reader) throws IOException {
-    if (reader instanceof BoboIndexReader){
-      final BoboIndexReader boboReader = (BoboIndexReader)reader;
-      FacetHandler facetHandler = boboReader.getFacetHandler(_column);
+    if (reader instanceof ZoieIndexReader){
+      ZoieIndexReader<BoboIndexReader> zoieReader = (ZoieIndexReader<BoboIndexReader>)reader;
+      List<BoboIndexReader> decorated = zoieReader.getDecoratedReaders();
       
-      if (facetHandler!=null){
-        DecimalFormat formatter = new DecimalFormat(DefaultSenseiInterpreter.DEFAULT_FORMAT_STRING_MAP.get(MetaType.Long));
-        BrowseSelection sel = new BrowseSelection(_column);
-        long duration = _dataUnit.convert(_nDays, TimeUnit.DAYS);
-        long now = _dataUnit.convert(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
-        long from = now - duration;
-        sel.addValue("["+formatter.format(from)+" TO *]");
-        return facetHandler.buildFilter(sel).getDocIdSet(boboReader);
+      List<DocIdSet> docIdSetList = new ArrayList<DocIdSet>(decorated.size());
+      for (BoboIndexReader bobo : decorated){
+        docIdSetList.add(buildFilterSet(bobo));
       }
-      throw new IllegalStateException("no facet handler defined with column: "+_column);
+      return new OrDocIdSet(docIdSetList);
     }
     else{
-      throw new IllegalStateException("reader not instance of "+BoboIndexReader.class);
+      throw new IllegalStateException("reader not instance of "+ZoieIndexReader.class);
     }
   }
 
