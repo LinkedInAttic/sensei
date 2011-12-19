e3a2d2f952b6985e1fa2b81c14eeb297b8823ca9
vzhabiuk <vzhabiuk@vzhabiuk-ld.linkedin.biz>
2011-12-18 19:53:09 -0800

    SENSEI-131 Implement method getDocuments by Ids in java client

--- clients/java-client/src/main/java/com/sensei/search/client/json/SenseiServiceProxy.java
+++ clients/java-client/src/main/java/com/sensei/search/client/json/SenseiServiceProxy.java
@@ -11,9 +11,15 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,35 +28,67 @@ import com.sensei.search.client.json.res.SenseiResult;
 
 public class SenseiServiceProxy {
     private static Logger LOG = Logger.getLogger(SenseiServiceProxy.class);
+    private  String host;
+    private  String port;
 
-    private String url;
 
 
-   public SenseiServiceProxy(String url) {
+
+   public SenseiServiceProxy(String host, String port) {
       super();
-      this.url = url;
+      this.host = host;
+      this.port = port;
+
     }
-   
-    public SenseiResult sendRequest( SenseiClientRequest request) throws IOException, JSONException {
+
+    public SenseiResult sendSearchRequest( SenseiClientRequest request) throws IOException, JSONException {
     	String requestStr = JsonSerializer.serialize(request).toString();
-        String output = sendPost( requestStr);
+        String output = sendPost(getSearchUrl(), requestStr);
         //System.out.println("Output from Server = " + output);
         return JsonDeserializer.deserialize(SenseiResult.class, jsonResponse(output));
     }
-    
-	  public String sendPost(String requestStr)
+    public List<Map<String, Object>> sendGetRequest(List<String> uids) throws IOException, JSONException {
+      List<Map<String, Object>> ret = new ArrayList<Map<String, Object>>(uids.size());
+      String response = sendPost(getStoreGetUrl(), new JSONArray(uids).toString());
+      if (response == null || response.length() == 0) {
+        return ret;
+      }
+      JSONArray jsonArray = new JSONArray(response);
+      for (int i = 0; i < jsonArray.length(); i++) {
+        Map<String, Object> item = new HashMap<String, Object>();
+        JSONObject jsonItem = jsonArray.optJSONObject(i);
+        if (jsonItem == null) {
+          continue;
+        }
+        Iterator keys = jsonItem.keys();
+        while (keys.hasNext()) {
+          String key = (String) keys.next();
+          item.put(key, jsonItem.opt(key));
+        }
+        ret.add(item);
+
+      }
+      return ret;
+    }
+    public String getSearchUrl() {
+      return "http://" + host + ":" + port + "/sensei";
+    }
+    public String getStoreGetUrl() {
+      return "http://" + host + ":" + port + "/sensei/get";
+    }
+	  public String sendPost(String urlStr, String requestStr)
 			throws MalformedURLException, IOException, ProtocolException,
 			UnsupportedEncodingException {
 		  HttpURLConnection conn = null;
         try {
         if (LOG.isInfoEnabled()){
-          LOG.info("Sending a post request to the server - " + this.url);
+          LOG.info("Sending a post request to the server - " + urlStr);
         }
-        
+
         if (LOG.isDebugEnabled()){
           LOG.debug("The request is - " + requestStr);
         }
-        URL url = new URL(this.url);
+        URL url = new URL(urlStr);
          conn = (HttpURLConnection) url.openConnection();
         conn.setDoOutput(true);
         conn.setRequestMethod("POST");
@@ -66,7 +104,7 @@ public class SenseiServiceProxy {
         os.flush();
         os.close();
         int responseCode = conn.getResponseCode();
-		
+
         if (LOG.isInfoEnabled()){
           LOG.info("The http response code is " + responseCode);
         }
@@ -75,7 +113,7 @@ public class SenseiServiceProxy {
                 + responseCode);
         }
         byte[] bytes = drain(new GZIPInputStream(new BufferedInputStream( conn.getInputStream())));
-        
+
         String output = new String(bytes, "UTF-8");
         if (LOG.isDebugEnabled()){
           LOG.debug("The response from the server is - " + output);
