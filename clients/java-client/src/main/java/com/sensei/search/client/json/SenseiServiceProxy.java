0329a4e9d28279624d7f3636c6c602c4b6bd29cf
vzhabiuk <vzhabiuk@vzhabiuk-ld.linkedin.biz>
2011-12-22 18:10:01 -0800

    Minor refactorings required for the perf project

--- clients/java-client/src/main/java/com/sensei/search/client/json/SenseiServiceProxy.java
+++ clients/java-client/src/main/java/com/sensei/search/client/json/SenseiServiceProxy.java
@@ -1,10 +1,13 @@
 package com.sensei.search.client.json;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,13 +17,13 @@ import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,33 +34,44 @@ import com.sensei.search.client.json.res.SenseiResult;
 public class SenseiServiceProxy {
     private static Log LOG = LogFactory.getLog(SenseiServiceProxy.class);
     private  String host;
-    private  String port;
+    private  int port;
     private static HttpClient httpclient;
 
     private static synchronized HttpClient getHttpClient() {
       if (httpclient == null) {
         ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager();
-        cm.setMaxTotal(100);
-        httpclient = new DefaultHttpClient(cm);
+        cm.setMaxTotal(200);
+        cm.setDefaultMaxPerRoute(100);
+        HttpParams params = new BasicHttpParams();
+        params.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 100);
+        params.setParameter(CoreConnectionPNames.TCP_NODELAY, false);
+        params.setParameter(CoreConnectionPNames.STALE_CONNECTION_CHECK, false);
+        params.setParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, Boolean.FALSE);
+        //params.setParameter("http.protocol.version", HttpVersion.HTTP_1_1);
+        httpclient = new DefaultHttpClient(cm, params);
+
       }
       return httpclient;
     }
 
-   public SenseiServiceProxy(String host, String port) {
-      super();
+   public SenseiServiceProxy(String host, int port) {
       this.host = host;
       this.port = port;
     }
 
-    public SenseiResult sendSearchRequest( SenseiClientRequest request) throws IOException, JSONException {
-    	String requestStr = JsonSerializer.serialize(request).toString();
-        String output = sendPost(getSearchUrl(), requestStr);
+    public SenseiResult sendSearchRequest( SenseiClientRequest request)  {
+    	try {
+      String requestStr = JsonSerializer.serialize(request).toString();
+        String output = sendPostRaw(getSearchUrl(), requestStr);
         //System.out.println("Output from Server = " + output);
         return JsonDeserializer.deserialize(SenseiResult.class, jsonResponse(output));
+    	} catch (Exception ex) {
+    	  throw new RuntimeException(ex);
+    	}
     }
     public List<Map<String, Object>> sendGetRequest(List<String> uids) throws IOException, JSONException {
       List<Map<String, Object>> ret = new ArrayList<Map<String, Object>>(uids.size());
-      String response = sendPost(getStoreGetUrl(), new JSONArray(uids).toString());
+      String response = sendPostRaw(getStoreGetUrl(), new JSONArray(uids).toString());
       if (response == null || response.length() == 0) {
         return ret;
       }
@@ -84,14 +98,16 @@ public class SenseiServiceProxy {
     public String getStoreGetUrl() {
       return "http://" + host + ":" + port + "/sensei/get";
     }
-	  public String sendPost(String urlStr, String requestStr) {
-			HttpPost httpPost = new HttpPost(urlStr);
+	  /*public String sendPost(String path, String requestStr) {
+			HttpPost httpPost = new HttpPost(path);
 			try {
 			httpPost.setHeader("Content-Type", "application/json; charset=utf-8");
 			httpPost.setHeader("Accept-Encoding", "gzip");
+			httpPost.setHeader("http.keepAlive", String.valueOf(true));
+			httpPost.setHeader("default", String.valueOf(true));
 			httpPost.setEntity(new StringEntity(requestStr));
-			 if (LOG.isInfoEnabled()){
-         LOG.info("Sending a post request to the server - " + urlStr);
+			 if (LOG.isDebugEnabled()){
+         LOG.debug("Sending a post request to the server - " + path);
        }
 
        if (LOG.isDebugEnabled()){
@@ -100,8 +116,8 @@ public class SenseiServiceProxy {
 			HttpResponse response = getHttpClient().execute(httpPost);
 			 int responseCode = response.getStatusLine().getStatusCode();
 
-       if (LOG.isInfoEnabled()){
-         LOG.info("The http response code is " + responseCode);
+       if (LOG.isDebugEnabled()){
+         LOG.debug("The http response code is " + responseCode);
        }
        if (responseCode != HttpURLConnection.HTTP_OK) {
            throw new IOException("Failed : HTTP error code : "
@@ -120,7 +136,7 @@ public class SenseiServiceProxy {
 			  httpPost.abort();
 			  throw new RuntimeException(ex);
 			}
-	}
+	}*/
     private JSONObject jsonResponse(String output) throws JSONException {
         return new JSONObject(output);
     }
@@ -141,9 +157,7 @@ public class SenseiServiceProxy {
     public void close() {
       getHttpClient().getConnectionManager().shutdown();
     }
-    /*public String sendPost(String urlStr, String requestStr)
-        throws MalformedURLException, IOException, ProtocolException,
-        UnsupportedEncodingException {
+    public String sendPostRaw(String urlStr, String requestStr){
         HttpURLConnection conn = null;
           try {
           if (LOG.isInfoEnabled()){
@@ -160,9 +174,13 @@ public class SenseiServiceProxy {
           conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
 
           conn.setRequestProperty("Accept-Encoding", "gzip");
+
          String string = requestStr;
           byte[] requestBytes = string.getBytes("UTF-8");
           conn.setRequestProperty("Content-Length", String.valueOf(requestBytes.length));
+          conn.setRequestProperty("http.keepAlive", String.valueOf(true));
+          conn.setRequestProperty("default", String.valueOf(true));
+
           //GZIPOutputStream zippedOutputStream = new GZIPOutputStream(conn.getOutputStream());
           OutputStream os = new BufferedOutputStream( conn.getOutputStream());
           os.write(requestBytes);
@@ -184,24 +202,12 @@ public class SenseiServiceProxy {
             LOG.debug("The response from the server is - " + output);
           }
           return output;
-          } finally {
+          } catch (Exception ex) {
+            throw new RuntimeException (ex);
+          }
+           finally {
             if (conn != null) conn.disconnect();
           }
     }
-      private JSONObject jsonResponse(String output) throws JSONException {
-          return new JSONObject(output);
-      }
-      byte[] drain(InputStream inputStream) throws IOException {
-          try {
-          byte[] buf = new byte[1024];
-          int len;
-          ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                  while ((len = inputStream.read(buf)) > 0) {
-                      byteArrayOutputStream.write(buf, 0, len);
-                  }
-          return byteArrayOutputStream.toByteArray();
-          } finally {
-              inputStream.close();
-          }
-      }*/
+
 }
--- clients/java-client/src/test/java/com/sensei/search/client/json/TweetsTest.java
+++ clients/java-client/src/test/java/com/sensei/search/client/json/TweetsTest.java
@@ -9,7 +9,7 @@ public static void main(String[] args) throws Exception {
 
   SenseiClientRequest request = SenseiClientRequest.builder().addSelection(Selection.terms("timeRange", "000120000")).
       addFacetInit("timeRange", "time", FacetInit.build("long", System.currentTimeMillis())).build();
-  SenseiServiceProxy senseiServiceProxy = new SenseiServiceProxy("localhost", "8080");
+  SenseiServiceProxy senseiServiceProxy = new SenseiServiceProxy("localhost", 8080);
   System.out.println(senseiServiceProxy.sendSearchRequest(request));
 }
 }
