c8a04cd0e0acac1e334bc656a379fe88c9f89355
John Wang <john.wang@gmail.com>
2012-01-04 17:40:54 -0800

    clean up

--- sensei-core/src/main/java/com/sensei/search/util/HDFSIndexCopier.java
+++ sensei-core/src/main/java/com/sensei/search/util/HDFSIndexCopier.java
@@ -1,6 +1,4 @@
 package com.sensei.search.util;
-<<<<<<< HEAD
-=======
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -17,7 +15,6 @@ package com.sensei.search.util;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
->>>>>>> 6b7b6c52c433a2ed5da87aa2c01772ff27083c85
 
 import java.net.URI;
 
