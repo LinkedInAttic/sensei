0cdc45a751c27eda28d26dfdcd5bef51b2236205
vzhabiuk <vzhabiuk@vzhabiuk-ld.linkedin.biz>
2012-02-06 10:37:13 -0800

    Fixed compilation errors caused be the incorrect bobo dependency version

--- sensei-core/src/test/java/com/senseidb/search/req/mapred/TestMapReduce.java
+++ sensei-core/src/test/java/com/senseidb/search/req/mapred/TestMapReduce.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Logger;
 
 import com.senseidb.search.req.mapred.impl.MapReduceBroker;
 import com.senseidb.search.req.mapred.impl.MapReduceRequest;
-import com.senseidb.search.req.mapred.impl.MapReduceResult;
+import com.senseidb.search.req.mapred.impl.SenseiMapReduceResult;
 import com.senseidb.svc.api.SenseiService;
 import com.senseidb.test.SenseiStarter;
 
@@ -22,17 +22,17 @@ public class TestMapReduce extends TestCase {
       httpRestSenseiService = SenseiStarter.httpRestSenseiService;
     }
     public void test1Max() throws Exception {
-      MapReduceResult result = mapReduceBroker.browse(new MapReduceRequest(new MaxMapReduce("groupid")));
+      SenseiMapReduceResult result = mapReduceBroker.browse(new MapReduceRequest(new MaxMapReduce("groupid")));
       assertEquals(14990L, (long)(Long)result.getReduceResult());
       System.out.println("!!!" + result.getTime());
     }
     public void test2GroupByColorAndGroupId() throws Exception {
-      MapReduceResult result = mapReduceBroker.browse(new MapReduceRequest(new CountGroupByMapReduce("color", "groupid")));
+      SenseiMapReduceResult result = mapReduceBroker.browse(new MapReduceRequest(new CountGroupByMapReduce("color", "groupid")));
       System.out.println("!!!" + result.getReduceResult());
       System.out.println("!!!" + result.getTime());
     }
     public void test3GroupByColorAndGroupId() throws Exception {      
-      MapReduceResult result = mapReduceBroker.browse(new MapReduceRequest(new CountGroupByMapReduce("color", "groupid")));
+      SenseiMapReduceResult result = mapReduceBroker.browse(new MapReduceRequest(new CountGroupByMapReduce("color", "groupid")));
       System.out.println("!!!" + result.getReduceResult());
       System.out.println("!!!" + result.getTime());
     }
