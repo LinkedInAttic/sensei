3b987ed76a2f00fe4f3998f7cd7241f865b86404
vzhabiuk <vzhabiuk@vzhabiuk-ld.linkedin.biz>
2012-02-13 11:44:10 -0800

    Fixed the problem with UID overflow, added the support for isNull predicate

--- clients/java/src/main/java/com/senseidb/search/client/json/req/filter/BoolFilter.java
+++ clients/java/src/main/java/com/senseidb/search/client/json/req/filter/BoolFilter.java
@@ -6,45 +6,53 @@ import com.senseidb.search.client.json.CustomJsonHandler;
 import com.senseidb.search.client.json.JsonField;
 
 /**
- *  <p>A filter that matches documents matching boolean combinations of other queries. Similar in concept to Boolean query, except that the clauses are other filters. Can be placed within queries that accept a filter.</p>
-<pre class="prettyprint lang-js"><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; </span><span class="str">"filtered"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"query"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"queryString"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"> <br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"default_field"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="str">"message"</span><span class="pun">,</span><span class="pln"> <br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"query"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="str">"elasticsearch"</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">},</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"filter"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"bool"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"must"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"term"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"> </span><span class="str">"tag"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="str">"wow"</span><span class="pln"> </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">},</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"must_not"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"range"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"age"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"> </span><span class="str">"from"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="lit">10</span><span class="pun">,</span><span class="pln"> </span><span class="str">"to"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="lit">20</span><span class="pln"> </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">},</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"should"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">[</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"term"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"> </span><span class="str">"tag"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="str">"sometag"</span><span class="pln"> </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">},</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">{</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="str">"term"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="pun">{</span><span class="pln"> </span><span class="str">"tag"</span><span class="pln"> </span><span class="pun">:</span><span class="pln"> </span><span class="str">"sometagtag"</span><span class="pln"> </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">]</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; &nbsp; &nbsp; </span><span class="pun">}</span><span class="pln"><br>&nbsp; &nbsp; </span><span class="pun">}</span><span class="pln"><br></span><span class="pun">}</span><span class="pln"> &nbsp; &nbsp;</span>
- *
+ * <p>
+ * A filter that matches documents matching boolean combinations of other
+ * queries. Similar in concept to Boolean query, except that the clauses are
+ * other filters. Can be placed within queries that accept a filter.
+ * </p>
+ * 
+ * 
  */
 @CustomJsonHandler(FilterJsonHandler.class)
-    public class BoolFilter implements Filter {
-        List<Filter> must;
-        List<Filter> must_not;
-        List<Filter> should;
-        @JsonField("minimum_number_should_match")
-        Boolean minimumNumberShouldMatch;
-        Double boost;
-        Boolean disableCoord;
-        public BoolFilter(List<Filter> must, List<Filter> must_not, List<Filter> should) {
-            super();
-            this.must = must;
-            this.must_not = must_not;
-            this.should = should;
-        }
+public class BoolFilter implements Filter {
+  List<Filter> must;
+  List<Filter> must_not;
+  List<Filter> should;
+  @JsonField("minimum_number_should_match")
+  Boolean minimumNumberShouldMatch;
+  Double boost;
+  Boolean disableCoord;
 
+  public BoolFilter(List<Filter> must, List<Filter> must_not, List<Filter> should) {
+    super();
+    this.must = must;
+    this.must_not = must_not;
+    this.should = should;
+  }
 
+  public List<Filter> getMust() {
+    return must;
+  }
 
-        public List<Filter> getMust() {
-            return must;
-        }
-        public void setMust(List<Filter> must) {
-            this.must = must;
-        }
-        public List<Filter> getMust_not() {
-            return must_not;
-        }
-        public void setMust_not(List<Filter> must_not) {
-            this.must_not = must_not;
-        }
-        public List<Filter> getShould() {
-            return should;
-        }
-        public void setShould(List<Filter> should) {
-            this.should = should;
-        }
+  public void setMust(List<Filter> must) {
+    this.must = must;
+  }
 
-   }
+  public List<Filter> getMust_not() {
+    return must_not;
+  }
+
+  public void setMust_not(List<Filter> must_not) {
+    this.must_not = must_not;
+  }
+
+  public List<Filter> getShould() {
+    return should;
+  }
+
+  public void setShould(List<Filter> should) {
+    this.should = should;
+  }
+
+}
