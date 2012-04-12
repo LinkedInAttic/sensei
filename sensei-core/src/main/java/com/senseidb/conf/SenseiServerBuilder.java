776e345e6f8fae90336c7c4e71ab8e1bcba33b36
Volodymyr Zhabiuk <vzhabiuk@linkedin.com>
2012-04-11 19:58:19 -0700

    Improved exception handling for the activity datastore

--- sensei-core/src/main/java/com/senseidb/conf/SenseiServerBuilder.java
+++ sensei-core/src/main/java/com/senseidb/conf/SenseiServerBuilder.java
@@ -410,8 +410,11 @@ public class SenseiServerBuilder implements SenseiConfParams{
       }
       zoieConfig.setReadercachefactory(readercachefactory);
       if (CompositeActivityManager.activitiesPresent(_senseiSchema)) {
-        activityManager = new CompositeActivityManager( _senseiConf.getString(SENSEI_INDEX_DIR), nodeid, _senseiSchema, zoieConfig.getVersionComparator());      
-      
+        try {
+    	  activityManager = new CompositeActivityManager( _senseiConf.getString(SENSEI_INDEX_DIR), nodeid, _senseiSchema, zoieConfig.getVersionComparator());      
+        } catch (Exception ex) {
+        	throw new ConfigurationException("Couldn't init the activity manager",ex);
+        }
       }
       List<FacetHandler<?>> facetHandlers = new LinkedList<FacetHandler<?>>();
       List<RuntimeFacetHandlerFactory<?,?>> runtimeFacetHandlerFactories = new LinkedList<RuntimeFacetHandlerFactory<?,?>>();
