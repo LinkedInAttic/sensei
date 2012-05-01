15e864b71a1efc0f94298d7fa0d92a81d644b84a
alperez <alperez@linkedin.com>
2012-05-01 11:08:40 -0700

    Fixing race condition in PluginHolder.

--- sensei-core/src/main/java/com/senseidb/plugin/PluginHolder.java
+++ sensei-core/src/main/java/com/senseidb/plugin/PluginHolder.java
@@ -4,50 +4,49 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 class PluginHolder {
-  private final SenseiPluginRegistry senseiPluginRegistry;
-  String pluginClass;
-  String pluginName;
-  String fullPrefix;
-  Object instance;
-  private Object factoryCreatedInstance;
-  Map<String, String> properties = new LinkedHashMap<String, String>();
+    private final SenseiPluginRegistry senseiPluginRegistry;
+    String pluginClass;
+    String pluginName;
+    String fullPrefix;
+    Object instance;
+    private Object factoryCreatedInstance;
+    Map<String, String> properties = new LinkedHashMap<String, String>();
 
-  public PluginHolder(SenseiPluginRegistry senseiPluginRegistry,
-                      String pluginClass,
-                      String pluginName,
-                      String fullPrefix) {
-    this.senseiPluginRegistry = senseiPluginRegistry;
-    this.pluginClass = pluginClass;
-    this.pluginName = pluginName;
-    this.fullPrefix = fullPrefix;
-  }
+    public PluginHolder(SenseiPluginRegistry senseiPluginRegistry,
+            String pluginClass,
+            String pluginName,
+            String fullPrefix) {
+        this.senseiPluginRegistry = senseiPluginRegistry;
+        this.pluginClass = pluginClass;
+        this.pluginName = pluginName;
+        this.fullPrefix = fullPrefix;
+    }
 
-  public Object getInstance() {
-    if (instance == null) {
-      synchronized (this) {
-        try {
-          instance = Class.forName(pluginClass).newInstance();
-          if (instance instanceof SenseiPlugin) {
-            ((SenseiPlugin) instance).init(properties, senseiPluginRegistry);
-            //((SenseiPlugin) instance).start();
-          }
-        } catch (Exception ex) {
-          throw new RuntimeException(ex);
+    public synchronized Object getInstance() {
+        if (instance == null) {
+            try {
+                instance = Class.forName(pluginClass).newInstance();
+                if (instance instanceof SenseiPlugin) {
+                    ((SenseiPlugin) instance).init(properties, senseiPluginRegistry);
+                    //((SenseiPlugin) instance).start();
+                }
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
         }
-      }
-    }
-    if (instance instanceof SenseiPluginFactory) {
-      if (factoryCreatedInstance == null) {
-        synchronized (instance) {
-          factoryCreatedInstance =
-            ((SenseiPluginFactory) instance).getBean(properties,
-                                                     fullPrefix,
-                                                     this.senseiPluginRegistry);
+        if (instance instanceof SenseiPluginFactory) {
+            if (factoryCreatedInstance == null) {
+                synchronized (instance) {
+                    factoryCreatedInstance =
+                        ((SenseiPluginFactory) instance).getBean(properties,
+                            fullPrefix,
+                            this.senseiPluginRegistry);
+                }
+            }
+            return factoryCreatedInstance;
+        } else {
+            return instance;
         }
-      }
-      return factoryCreatedInstance;
     }
-    return instance;
-  }
 
 }
