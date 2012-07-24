403667e2d8966a0bf178f957f9d994fb92baa6c5
Terence Yim <tyim@linkedin.com>
2012-07-24 12:06:14 -0700

    Use _zoieSystemMap.keySet instead as the _dataCollectorMap is not populated yet.

--- sensei-core/src/main/java/com/senseidb/indexing/DefaultStreamingIndexingManager.java
+++ sensei-core/src/main/java/com/senseidb/indexing/DefaultStreamingIndexingManager.java
@@ -159,7 +159,7 @@ public class DefaultStreamingIndexingManager implements SenseiIndexingManager<JS
 		StreamDataProvider<JSONObject> dataProvider = null;
     if (_gateway!=null){
 		  try{
-		    dataProvider = _gateway.buildDataProvider(_senseiSchema, _oldestSinceKey, pluginRegistry,_shardingStrategy,_dataCollectorMap.keySet());
+		    dataProvider = _gateway.buildDataProvider(_senseiSchema, _oldestSinceKey, pluginRegistry,_shardingStrategy,_zoieSystemMap.keySet());
         long maxEventsPerMin = _myconfig.getLong(EVTS_PER_MIN,40000);
         dataProvider.setMaxEventsPerMinute(maxEventsPerMin);
         int batchSize = _myconfig.getInt(BATCH_SIZE,1);
